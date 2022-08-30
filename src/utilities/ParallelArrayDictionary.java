96634730a354bb684bfebe65fe025ddb9f805496
jackecrouse <112016157+jackecrouse@users.noreply.github.com>
2022-08-30 18:15:48 -0400

    some tests :)))))))

--- src/utilities/ParallelArrayDictionary.java
+++ src/utilities/ParallelArrayDictionary.java
@@ -1,11 +1,134 @@
 package utilities;
 
-public class ParallelArrayDictionary
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class ParallelArrayDictionary<Key, Value> implements Map<Key, Value>
 {
+	protected ArraySet<Key> _keys;
+	protected ArraySet<Value> _values;
+	
 
 	public ParallelArrayDictionary()
 	{
-		// TODO Auto-generated constructor stub
+		_keys = new ArraySet<Key>();
+		_values = new ArraySet<Value>(); 
 	}
+
+
+	@Override
+	public int size() {
+
+		return _keys.size();
+	}
+
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return _keys.isEmpty(); 
+	}
+
+
+	@Override
+	public boolean containsKey(Object key) {
+
+		return _keys.contains(key);
+	}
+
+
+	@Override
+	public boolean containsValue(Object value) {
+
+		return _values.contains(value);
+	}
+
+
+	@Override
+	public Value get(Object key) {
+		// TODO Auto-generated method stub
+
+		int index = _keys.indexOf(key);
+		return _values.get(index);
+	}
+	
+	
+
+	@Override
+	public Value put(Key key, Value value) {
+
+		int index = _keys.indexOf(key);
+		
+		if(index == -1)
+		{
+			_keys.add(key);
+			_values.add(value);
+			
+			return null; 
+		}
+
+		Value oldVal = _values.get(index);
+		_values.set(index, value);
+		return oldVal;
+	}
+
+
+	@Override
+	public Value remove(Object key) {
+		
+		int index = _keys.indexOf(key);
+		
+		if(index == -1)
+		{
+			return null;
+		}
+		
+		Value oldVal = _values.get(index);
+		
+		_keys.remove(index);
+		_values.remove(index);
+		
+		return oldVal;
+		
+	}
+
+
+	@Override
+	public void putAll(Map<? extends Key, ? extends Value> m) {
+		
+		for(Key key : m.keySet())
+		{
+			this.put(key, m.get(key));
+		}
+	}
+
+
+	@Override
+	public void clear() {
+		_keys.clear();
+		_values.clear();
+	}
+
+
+	@Override
+	public Set<Key> keySet() {return _keys;}
+
+	@Override
+	public Collection<Value> values() {
+		// TODO Auto-generated method stub
+		return _values; 		//(Collection<Value>) _values;
+	}
+
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Set<Entry<Key, Value>> entrySet() {
+		// TODO Auto-generated method stub
+		return (Set<Entry<Key, Value>>) this;
+	}
+	
+	
+	
 
 }
--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -1,17 +1,64 @@
 package utilities;
 
+import static org.junit.Assert.*;
+
 import org.junit.jupiter.api.Test;
 
-class ArraySetTest
+class ArraySetTest<E>
 {
+	
+	@SuppressWarnings("unchecked")
+	ArraySet<Integer> populate(int low, int high)
+	{
+		ArraySet<Integer> arrySet = new ArraySet<Integer>();
+		
+		for(Integer i = low; i <= high; i++)
+		{
+			arrySet.add(i);
+		}
+		
+		return arrySet;
+	}
+	
+	String toString(ArraySet<Integer> arry)
+	{
+		String res = "[";
+		
+		for(Integer item: arry)
+		{
+			res += item + ", ";
+		}
+		
+		res = res.substring(0, res.length() -2);
+		return res += "]";
+		
+	}
+	
+	
 	@Test
 	void testArraySetCollectionOfE()
 	{
+		
 	}
 
 	@Test
 	void testAddE()
 	{
+		ArraySet<Integer> emptySet = new ArraySet<Integer>();
+		ArraySet<Integer> fullSet = populate(1, 10); 
+		
+		assertFalse(fullSet.add(10));
+		assertTrue(fullSet.add(11));
+		assertTrue(emptySet.add(1));
+		
+		emptySet.add(1);
+		emptySet.add(2);
+		emptySet.add(3);
+		emptySet.add(3);
+		emptySet.add(null);
+		emptySet.add(null);
+			
+		assertEquals("[1, 2, 3, null]", toString(emptySet));
 	}
 
 	@Test
@@ -22,6 +69,20 @@ class ArraySetTest
 	@Test
 	void testRetainAll()
 	{
+		ArraySet<Integer> fullSet = populate(3, 7); 
+		ArraySet<Integer> subSet = populate(4, 6); 
+		
+		fullSet.retainAll(subSet);
+		
+		assertEquals(toString(fullSet), "[4, 5, 6]");
+		
+		subSet = populate(1,4);
+		
+		fullSet.retainAll(subSet);
+		
+		assertEquals(toString(fullSet), "[4]");
+		
+
 	}
 
 	@Test
